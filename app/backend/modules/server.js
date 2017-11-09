d6f7e6827e0d6b17a10c7fb2757c70cd8507a2d6
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-09 16:04:06 +0000

    Add server module

---
+++ app/backend/modules/server.js
@@ -0,0 +1,112 @@
+'use strict';
+
+/*
+ * SERVER
+ */
+
+const config = require(`config-ninja`).use(`eyewitness-ui`);
+
+const path = require(`path`);
+const bodyParser = require(`body-parser`);
+const compression = require(`compression`);
+const cookieParser = require(`cookie-parser`);
+const express = require(`express`);
+const basicAuth = require(`express-basic-auth`);
+const exphbs  = require(`express-handlebars`);
+const HomeController = require(`../controllers/home`);
+const middleware = require(`./middleware`);
+
+/*
+ * Starts the server on the given port.
+ */
+async function start (port) {
+
+	const app = express();
+
+	setupServerViewEngine(app);
+	setupServerMiddleware(app);
+	setupServerRoutes(app);
+	setupServerErrorHandling(app);
+
+	// Start listening.
+	await new Promise((resolve, reject) =>
+		app.listen(port, err => (err ? reject(err) : resolve()))
+	);
+
+}
+
+/*
+ * Configure the view engine.
+ */
+function setupServerViewEngine (app) {
+
+	app.set(`views`, path.join(__dirname, `..`, `..`, `frontend`, `build`, `views`));
+
+	app.engine(`.handlebars.html`, exphbs({
+		extname: `.handlebars.html`,
+		compilerOptions: {
+			preventIndent: true,
+		},
+		helpers: {
+
+		},
+	}));
+
+	app.set(`view engine`, `.handlebars.html`);
+
+}
+
+/*
+ * Add all of the routes.
+ */
+function setupServerMiddleware (app) {
+
+	// General middleware.
+	app.use(compression({ threshold: 0 }));
+	app.use(cookieParser(config.authentication.cookie.secret));
+	app.use(bodyParser.urlencoded({  // This will let us get the data from a POST.
+		extended: true,
+	}));
+	app.use(bodyParser.json());
+	app.use(basicAuth({ users: config.authentication.basicAuth.users }));
+
+	// Static files (must come before logging to avoid logging out every request for a static file e.g. favicon).
+	const staticDirectory = path.join(__dirname, `..`, `..`, `frontend`, `build`, `static`);
+	app.use(express.static(staticDirectory));
+
+	// Temporary favicon.
+	app.use(`/favicon.ico`, (req, res) => res.status(404).end(`NOT_ADDED_YET`));
+
+	// Health check endpoint.
+	app.use(`/health-check`, middleware.healthCheck);
+
+	// Redirect to HTTPS.
+	app.use(middleware.enforceHttps);
+
+}
+
+/*
+ * Add all of the routes.
+ */
+function setupServerRoutes (app) {
+
+	const ctrlHome = new HomeController();
+	app.get(`*`, ctrlHome.renderHomePage.bind(ctrlHome));
+
+}
+
+/*
+ * Add all the error handling middleware which must go after the routes.
+ */
+function setupServerErrorHandling (app) {
+
+	app.use(middleware.handleErrors);
+
+}
+
+/*
+ *
+ */
+module.exports = {
+	start,
+};
