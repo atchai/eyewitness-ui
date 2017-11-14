edf0b62f3729c5952a48bb2c0509fa3240903fe6
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-14 15:30:30 +0000

    Tidy up main entry point

--- app/frontend/source/scripts/main.js
+++ app/frontend/source/scripts/main.js
@@ -2,15 +2,24 @@
  * FRONTEND ENTRY POINT
  */
 
-/* eslint-disable node/no-unpublished-require */
+/* eslint-plugin-disable node */
 
+/*
+ * Import the styles.
+ */
 import 'normalize.css';
 import '../styles/main.scss';
 
+/*
+ * Import the application.
+ */
 import Vue from 'vue';
 import App from '../vue/App';
 
+/*
+ * Create a new Vue application instance.
+ */
 new Vue({
-	el: `#app`,  // eslint-disable-line id-length
-	render: createElement => createElement(App),
+	el: `#app`, // eslint-disable-line id-length
+	render: createElement => createElement(App), // We must use render because we don't bundle in the Vue compiler.
 });
