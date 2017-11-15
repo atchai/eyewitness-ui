56be29ae370906536e8ea5c5936dd1ff5aec5f70
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-15 11:03:58 +0000

    Scope the component CSS

--- app/frontend/source/components/App.vue
+++ app/frontend/source/components/App.vue
@@ -28,7 +28,7 @@
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 	#app {
 		display: flex;
--- app/frontend/source/components/chrome/HeaderBar.vue
+++ app/frontend/source/components/chrome/HeaderBar.vue
@@ -45,7 +45,7 @@
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 
 	header {
 		display: flex;
