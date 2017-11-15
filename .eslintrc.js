f91ebb2a4287f7a9e13e88853ac76eb87ca59626
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-15 10:56:06 +0000

    Move eslint-plugin-disable config to root eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,13 @@
 module.exports = {
   "extends": "eslint-config-recombix",
+  "settings": {
+    "eslint-plugin-disable": {
+      "paths": {
+        "node": ["**/app/frontend/**/*"]
+      },
+      "extensions": [".js", ".vue"]
+    }
+  },
   "rules": {
     "no-process-exit": 0,
   },
--- app/frontend/source/.eslintrc.js
+++ app/frontend/source/.eslintrc.js
@@ -3,14 +3,6 @@ module.exports = {
   "parserOptions": {
     "sourceType": "module"
   },
-  "settings": {
-    "eslint-plugin-disable": {
-      "paths": {
-        "node": ["*"]
-      },
-      "extensions": [".js", ".vue"]
-    }
-  },
   "rules": {
     "filenames/match-regex": [2, /^(?:[a-zA-Z][a-z0-9]+)*$/g],  // Allows "HeaderBar.vue" or "someFile.js".
   },
