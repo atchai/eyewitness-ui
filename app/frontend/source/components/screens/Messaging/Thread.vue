d337f11d01e50537d1d2dff3a6b9e08aa3379fc3
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-15 14:28:30 +0000

    Add placeholder thread component

---
+++ app/frontend/source/components/screens/Messaging/Thread.vue
@@ -0,0 +1,32 @@
+<!--
+	THREAD
+-->
+
+<template>
+
+	<div class="thread">
+		Thread...
+	</div>
+
+</template>
+
+<script>
+
+	export default {
+		data: function () {
+			return {
+
+			};
+		},
+	};
+
+</script>
+
+<style lang="scss" scoped>
+
+	.thread {
+		height: 4.50rem;
+		border-bottom: 1px solid $panel-border-color;
+	}
+
+</style>
