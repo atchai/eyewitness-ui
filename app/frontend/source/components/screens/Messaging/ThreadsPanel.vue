d062b1c9fd2b3fdbaad7b7ce33d268a6f6f7c9b5
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-15 14:29:03 +0000

    Continue work on threads panel

--- app/frontend/source/components/screens/Messaging/ThreadsPanel.vue
+++ app/frontend/source/components/screens/Messaging/ThreadsPanel.vue
@@ -5,12 +5,16 @@
 <template>
 
 	<div class="panel">
-		Threads...
+		<div class="inbox">Inbox</div>
+		<div class="threads">
+		</div>
 	</div>
 
 </template>
 
 <script>
+
+	import Thread from './Thread';
 
 	export default {
 		data: function () {
@@ -18,6 +22,7 @@
 
 			};
 		},
+		components: { Thread },
 	};
 
 </script>
@@ -25,10 +30,27 @@
 <style lang="scss" scoped>
 
 	.panel {
+		display: flex;
+		flex-direction: column;
 		flex-shrink: 0;
 		width: 20.00rem;
 		background: $panel-background-color;
 		border-right: 1px solid $panel-border-color;
+
+		>.inbox {
+			height: 5.00rem;
+			line-height: 5.00rem;
+			text-align: center;
+			text-transform: uppercase;
+			color: $panel-grey-text;
+			letter-spacing: 1px;
+			border-bottom: 1px solid $panel-border-color;
+		}
+
+		>.threads {
+			flex: 1;
+			@include scroll-vertical();
+		}
 	}
 
 </style>
