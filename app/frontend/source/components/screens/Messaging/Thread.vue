8fd141fd5b8545e48918a010708778d075401149
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-16 15:36:12 +0000

    Continue work on thread component

--- app/frontend/source/components/screens/Messaging/Thread.vue
+++ app/frontend/source/components/screens/Messaging/Thread.vue
@@ -4,8 +4,15 @@
 
 <template>
 
-	<div class="thread">
-		Thread...
+	<div :class="{ thread: true, unread: unread }">
+		<div class="image"><img src="imageUrl"></div>
+		<div class="meta">
+			<div class="details">
+				<div class="name">{{ name }}</div>
+				<div class="date">{{ date }}</div>
+			</div>
+			<div class="message">{{ message }}</div>
+		</div>
 	</div>
 
 </template>
@@ -13,6 +20,7 @@
 <script>
 
 	export default {
+		props: [`imageUrl`, `name`, `date`, `message`, `unread`],
 		data: function () {
 			return {
 
@@ -25,8 +33,61 @@
 <style lang="scss" scoped>
 
 	.thread {
+		display: flex;
+		flex-direction: row;
 		height: 4.50rem;
+		padding: 0 0.50rem;
 		border-bottom: 1px solid $panel-border-color;
+
+		>.image {
+			display: flex;
+			flex-shrink: 0;
+			width: 3.50rem;
+			margin-right: 0.50rem;
+
+			img {
+				display: block;
+				margin: auto;
+				width: 3.00rem;
+				height: 3.00rem;
+				border-radius: 50%;
+				overflow: hidden;
+			}
+		}
+
+		>.meta {
+			display: flex;
+			flex-direction: column;
+			flex: 1;
+			margin: 0.35rem 0;
+			font-size: 0.80rem;
+
+			>.details {
+				display: flex;
+				flex-direction: row;
+				flex-shrink: 0;
+				height: 1.25rem;
+
+				>.name {
+					flex: 1;
+					font-weight: bold;
+				}
+
+				>.date {
+					flex-shrink: 0;
+					width: 3.00rem;
+					color: $faded-color;
+					text-align: right;
+				}
+			}
+
+			>.message {
+				flex: 1;
+				min-height: 0;
+				color: $panel-grey-text;
+				overflow: hidden;
+			}
+		}
 	}
 
 </style>
