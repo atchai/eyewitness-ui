1a563d5d55794c146e2242941459c07bc4850b3f
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-19 22:58:19 +0000

    Continue work on settings screen

--- app/frontend/source/components/screens/Settings/Settings.vue
+++ app/frontend/source/components/screens/Settings/Settings.vue
@@ -7,21 +7,52 @@
 	<div class="screen padding">
 		<ScreenHeader
 			title="Settings"
+			description="Welcome messages:"
 		/>
+		<div class="welcome">
+			<WelcomeMessage
+				v-for="welcomeMessage in welcomeMessages"
+				:key="welcomeMessage.welcomeMessageId"
+				:welcomeMessageId="welcomeMessage.welcomeMessageId"
+				:text="welcomeMessage.text"
+			/>
+		</div>
+		<div class="actions">
+			<button class="primary" @click="addWelcomeMessage">Add Message</button>
+		</div>
 	</div>
 
 </template>
 
 <script>
 
+	import mongoose from 'mongoose';
 	import ScreenHeader from '../../common/ScreenHeader';
+	import WelcomeMessage from './WelcomeMessage';
 
 	export default {
-		components: { ScreenHeader },
+		computed: {
+			welcomeMessages () { return this.$store.state.welcomeMessages; },
+		},
+		components: { ScreenHeader, WelcomeMessage },
+		methods: {
+
+			addWelcomeMessage () {
+				this.$store.commit(`add-welcome-message`, {
+					welcomeMessageId: new mongoose.mongo.ObjectId(),
+					text: ``,
+				});
+			},
+
+		}
 	};
 
 </script>
 
 <style lang="scss" scoped>
+
+	.screen {
+
+	}
 
 </style>
---
+++ app/frontend/source/components/screens/Settings/WelcomeMessage.vue
@@ -0,0 +1,54 @@
+<!--
+	WELCOME MESSAGE
+-->
+
+<template>
+
+	<div class="welcome-message">
+		<textarea>{{ text }}</textarea>
+		<div class="actions">
+			<button @click="removeWelcomeMessage(welcomeMessageId)">Delete</button>
+			<button class="primary" @click="saveWelcomeMessage(welcomeMessageId, $event)">Edit</button>
+		</div>
+	</div>
+
+</template>
+
+<script>
+
+	export default {
+		props: [`welcomeMessageId`, `text`],
+		methods: {
+
+			removeWelcomeMessage (key) {
+				this.$store.commit(`remove-welcome-message`, { key });
+			},
+
+			saveWelcomeMessage (welcomeMessageId, event) {
+
+			},
+
+		},
+	};
+
+</script>
+
+<style lang="scss" scoped>
+
+	.welcome-message {
+		margin: 2.00rem 0;
+
+		>textarea {
+			min-width: 100%;
+			max-width: 100%;
+			min-height: 5.00rem;
+			max-height: 10.00rem;
+		}
+
+		>.actions {
+			margin-top: 1.00rem;
+			text-align: right;
+		}
+	}
+
+</style>
