eaa5f5b69824603a4fa059d99491ca4bfe5cb27d
Josh Cole <saikojosh@users.noreply.github.com>
2017-11-19 22:57:53 +0000

    Update store

--- app/frontend/source/scripts/store.js
+++ app/frontend/source/scripts/store.js
@@ -4,7 +4,7 @@
 
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { updateStoreObjectItem } from './utilities';
+import { updateStoreObjectItem, removeStoreObjectItem } from './utilities';
 
 Vue.use(Vuex);
 
@@ -12,18 +12,22 @@ export default new Vuex.Store({
 	state: {
 		threads: {},
 		articles: {},
+		welcomeMessages: {},
 		settings: {
 			showStories: null,
-			welcomeMessages: [],
 		},
 	},
 	mutations: {
 		'update-thread': (state, payload) => updateStoreObjectItem(state.threads, payload),
 		'update-threads': (state, payload) => state.threads = payload,
+		'remove-thread': (state, payload) => removeStoreObjectItem(state.threads, payload),
 		'update-article': (state, payload) => updateStoreObjectItem(state.articles, payload),
 		'update-articles': (state, payload) => state.articles = payload,
-		'set-show-stories': (state, payload) => state.settings.showStories = payload,
-		'update-welcome-message': (state, payload) => updateStoreObjectItem(state.settings.welcomeMessages, payload),
-		'update-welcome-messages': (state, payload) => state.settings.welcomeMessages = payload,
+		'remove-article': (state, payload) => removeStoreObjectItem(state.articles, payload),
+		'set-show-stories': (state, payload) => state.showStories = payload,
+		'add-welcome-message': (state, payload) => state.welcomeMessages[payload.welcomeMessageId] = payload,
+		'update-welcome-message': (state, payload) => updateStoreObjectItem(state.welcomeMessages, payload),
+		'update-welcome-messages': (state, payload) => state.welcomeMessages = payload,
+		'remove-welcome-message': (state, payload) => removeStoreObjectItem(state.welcomeMessages, payload),
 	},
 });
