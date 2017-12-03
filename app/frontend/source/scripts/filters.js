0d6c46958eb051a6844c353245769cdd0d02b8e1
Josh Cole <saikojosh@users.noreply.github.com>
2017-12-03 13:04:29 +0000

    Add filter to format dates as relative to now

--- app/frontend/source/scripts/filters.js
+++ app/frontend/source/scripts/filters.js
@@ -12,3 +12,11 @@ Vue.filter(`formatDate`, (value, format) => {
 	const date = moment(value);
 	return date.format(format);
 });
+
+/*
+ * Allows dates to be formatted using momentjs relative to the current date and time.
+ */
+Vue.filter(`formatDateAsRelative`, (value) => {
+	const date = moment(value);
+	return date.from(moment());
+});
