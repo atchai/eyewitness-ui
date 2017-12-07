6cad107ba49d1599c261c4e315afee2d54452a90
Josh Cole <saikojosh@users.noreply.github.com>
2017-12-07 19:31:06 +0000

    Return JSON errors if the request was JSON

--- app/backend/modules/middleware.js
+++ app/backend/modules/middleware.js
@@ -28,8 +28,21 @@ function enforceHttps (req, res, next) {
  * Handles errors in any of the routes.
  */
 function handleErrors (err, req, res, next) { // eslint-disable-line no-unused-vars
+
 	console.error(`A controller error occured:\n`, err.stack); // eslint-disable-line no-console
-	return res.status(500).end(`A controller error occured: "${err}".`);
+
+	// The HTTP status code should be 200 if we are returning JSON.
+	if (req.headers[`content-type`] === `application/json`) {
+
+		return res.json({
+			success: false,
+			error: err.message,
+		});
+
+	}
+
+	return res.status(500).end(`A controller error occured because of "${err}".`);
+
 }
 
 /*
