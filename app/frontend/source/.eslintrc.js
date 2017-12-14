8bfcf4650bdf4c5c3e7877bcff48db79120b8650
Josh Cole <saikojosh@users.noreply.github.com>
2017-12-14 14:47:39 +0000

    Allow APP_CONFIG in eslint

--- app/frontend/source/.eslintrc.js
+++ app/frontend/source/.eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   "parserOptions": {
     "sourceType": "module"
   },
+  "globals": {
+    "APP_CONFIG": true
+  },
   "rules": {
     "filenames/match-regex": [2, /^(?:[a-zA-Z][a-z0-9]+)*$/g],  // Allows "HeaderBar.vue" or "someFile.js".
   },
