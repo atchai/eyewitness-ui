672854ce46392a3a1b50a88aea6ca40b0a5882a2
Josh Cole <saikojosh@users.noreply.github.com>
2018-01-23 17:52:34 +0000

    Fix hover message timestamp

--- app/frontend/source/components/screens/Messaging/Message.vue
+++ app/frontend/source/components/screens/Messaging/Message.vue
@@ -5,7 +5,7 @@
 <template>
 
 	<div :data-item-id="itemId" :class="[`message`, direction]">
-		<div class="bubble" :title="sentAt | formatDate('DD/MM/YYYY HH:MM')">
+		<div class="bubble" :title="sentAt | formatDate('DD/MM/YYYY HH:mm')">
 
 			<div v-if="data.text && !(data.attachments && data.attachments.length)" class="inner text">
 				{{data.text}}
