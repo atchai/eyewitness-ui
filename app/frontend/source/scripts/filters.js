30bf0dcfe919602d8a3a8bb975248042412bbcae
Justin Emery <justin.emery@gmail.com>
2018-05-11 17:44:12 +0100

    Update tip -> story

--- app/frontend/source/scripts/filters.js
+++ app/frontend/source/scripts/filters.js
@@ -26,7 +26,7 @@ Vue.filter(`formatDateAsRelative`, (value) => {
  */
 Vue.filter(`enquiryType`, (value) => {
 	switch (value) {
-		case `submit a tip`: return `-story-`;
+		case `submit a story`: return `-story-`;
 		case `advertise`: return `-advertise-`;
 		case `other`: return `-other-`;
 		default: return ``;
