d507f6b05f1caf264bae8ca607b1b59621ffecbd
Josh Cole <saikojosh@users.noreply.github.com>
2018-06-11 16:38:56 +0100

    Update UI on memory change

--- app/frontend/source/scripts/webSocketClient.js
+++ app/frontend/source/scripts/webSocketClient.js
@@ -11,34 +11,42 @@ let socket;
  */
 function ensureFullThread (store, threadId) {
 
-	// Nothing to do if we already have the thread.
-	const hasThread = store.getters.hasThread(threadId);
-	if (hasThread) { return true; }
+	return new Promise((resolve, reject) => {
 
-	// Request the thread data from the backend.
-	socket.emit(
-		`messaging/thread/get-info`,
-		{ itemId: threadId },
-		resData => {
+		// Nothing to do if we already have the thread.
+		const hasThread = store.getters.hasThread(threadId);
 
-			if (!resData || !resData.success) {
-				alert(`There was a problem loading in a new thread.`);
-				return;
-			}
-
-			// Add the new thread.
-			store.commit(`add-thread`, {
-				key: resData.thread.itemId,
-				data: resData.thread,
-				sortField: `latestDate`,
-				sortDirection: `desc`,
-			});
-
+		if (hasThread) {
+			resolve();
+			return;
 		}
-	);
 
-	// We need to wait for the thread.
-	return false;
+		// Request the thread data from the backend.
+		socket.emit(
+			`messaging/thread/get-info`,
+			{ itemId: threadId },
+			resData => {
+
+				if (!resData || !resData.success) {
+					alert(`There was a problem loading in a new thread.`);
+					reject();
+					return;
+				}
+
+				// Add the new thread.
+				store.commit(`add-thread`, {
+					key: resData.thread.itemId,
+					data: resData.thread,
+					sortField: `latestDate`,
+					sortDirection: `desc`,
+				});
+
+				resolve();
+
+			}
+		);
+
+	});
 
 }
 
@@ -57,13 +65,12 @@ function isThreadVisible (router, threadId) {
 /*
  * Handles new messages pushed to the frontend from the backend.
  */
-function handleNewMessages (store, router, data) {
+async function handleNewMessages (store, router, data) {
 
 	const threadId = data.thread.itemId;
 
 	// If we have not spoken to this user before we must ask the backend for the full thread data.
-	const hasThread = ensureFullThread(store, threadId);
-	if (!hasThread) { return; }
+	await ensureFullThread(store, threadId);
 
 	// Add the new message to the store if the thread is currently visible.
 	if (isThreadVisible(router, threadId)) {
@@ -75,7 +82,7 @@ function handleNewMessages (store, router, data) {
 
 	// Update thread meta.
 	store.commit(`update-thread`, {
-		key: data.thread.itemId,
+		key: threadId,
 		dataFunction: (stateProperty) => {
 			stateProperty.latestDate = data.thread.latestDate;
 			stateProperty.latestMessage = data.thread.latestMessage;
@@ -90,33 +97,45 @@ function handleNewMessages (store, router, data) {
 /*
  * Handles memory changes pushed to the frontend from the backend.
  */
-function handleMemoryChanges (store, router, data) {
+async function handleMemoryChanges (store, router, data) {
 
 	const threadId = data.thread.itemId;
-
-	// If we have not spoken to this user before we must ask the backend for the full thread data.
-	const hasThread = ensureFullThread(store, threadId);
-	if (!hasThread) { return; }
-
 	const { key, operation, newValue } = data.memory;
 	const keyFormatted = key.replace(/^appData\./i, ``);
 
-	if (operation === `set`) {
+	// If we have not spoken to this user before we must ask the backend for the full thread data.
+	await ensureFullThread(store, threadId);
 
-		const action = (store.getters.hasBotMemory(keyFormatted) ? `update-bot-memory` : `add-bot-memory`);
+	// Update the bot memory if its thread is currently visible.
+	if (isThreadVisible(router, threadId)) {
 
-		store.commit(action, {
-			key: keyFormatted,
-			data: newValue,
-		});
+		if (operation === `set`) {
+
+			const action = (store.getters.hasBotMemory(keyFormatted) ? `update-bot-memory` : `add-bot-memory`);
+
+			store.commit(action, {
+				key: keyFormatted,
+				data: newValue,
+			});
+
+		}
+		else {
+
+			store.commit(`remove-bot-memory`, {
+				key: keyFormatted,
+			});
+
+		}
 
 	}
-	else {
 
-		store.commit(`remove-bot-memory`, {
-			key: keyFormatted,
+	// Update thread meta when certain memory changes are detected.
+	if ([ `enquiryType`, `conversationState` ].includes(keyFormatted)) {
+		store.commit(`update-thread`, {
+			key: threadId,
+			dataField: keyFormatted,
+			dataValue: newValue,
 		});
-
 	}
 
 }
